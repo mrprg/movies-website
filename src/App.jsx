b9a8288ce790862ea1d6c941ef0cd7c16e26288c
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-04 22:50:53 +0330

    add MovieList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MovieList from "./components/MovieList";
 const App = () => {
-  const [movies, setMovies] = useState([]); 
+  const [movies, setMovies] = useState([
+    {
+      Title: "The Avengers",
+      Year: "2012",
+      imdbID: "tt0848228",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    },
+  ]);
   return (
     <div className="container-fluid movie-app">
-        <div className="row">
-            <MovieList
-            movies={movies}
-            />
-        </div>
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 };
