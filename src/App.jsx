132d55d2b38b457872f66ecfa53fb9589cf3362b
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-12 18:12:33 +0330

    implementing Search Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css";
 import MovieList from "./components/MovieList";
+import SearchBox from "./components/SearchBox";
+import MovieListHeader from "./components/MovieListHeader";
 const App = () => {
-  const [movies, setMovies] = useState([
-    {
-      Title: "The Avengers",
-      Year: "2012",
-      imdbID: "tt0848228",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=79555010`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(()=> {
+    getMovieRequest(searchValue);
+  }, [searchValue])
   return (
     <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeader heading="Movies" />
+        <SearchBox
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
